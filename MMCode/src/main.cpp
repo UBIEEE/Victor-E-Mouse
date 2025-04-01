e2a11b639e1dcf74a5be9de1b33f8016ac5a5151
folgakyle <folgakyle@gmail.com>
2025-04-01 18:06:32 -0400

    sensor class implementation

--- MMCode/src/main.cpp
+++ MMCode/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <sensor.h>
 
 /*
 created by Rui Santos,
@@ -78,63 +79,6 @@ void loop() {
   // Update previousStateCLK with the current state
   previousStateCLK = currentStateCLK;
 }
-
-class Sensor
-{
-    public:
-    int trigPin;
-    int echoPin;
-    long duration;
-    long cm;
-    long inches;
-
-    Sensor(int tPin, int ePin)
-    {
-        trigPin = tPin;
-        echoPin = ePin;
-    }
-
-    int getTrig()
-    {
-        return trigPin;
-    }
-
-    int getEcho()
-    {
-        return echoPin;
-    }
-
-    long getDuration()
-    {
-        return duration;
-    }
-
-    void getReading()
-    {
-        // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
-        // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-        digitalWrite(trigPin, LOW);
-        delayMicroseconds(2);
-        digitalWrite(trigPin, HIGH);
-        delayMicroseconds(10);
-        digitalWrite(trigPin, LOW);
-
-        // Read the signal from the sensor: a HIGH pulse whose
-        // duration is the time (in microseconds) from the sending
-        // of the ping to the reception of its echo off of an object.
-        duration = pulseIn(echoPin, HIGH);
-    
-        // Convert the time into a distance
-        cm = (duration / 2) / 29.1;   // Divide by 29.1 or multiply by 0.0343
-        inches = (duration / 2) / 74; // Divide by 74 or multiply by 0.0135
-        Serial.print("in ");
-        Serial.print(inches);
-        Serial.print(" cm ");
-        Serial.print(cm);
-        Serial.println();
-        delay(250);
-    }
-};
 
 Sensor left(11, 12);
 Sensor right(9, 10);
