dc237ece28cae080d41a0025232a4f3288e9b23c
Conor74 <crswete@buffalo.edu>
2025-04-15 20:22:09 -0400

    cell class

---
+++ MMCode/src/cell.cpp
@@ -0,0 +1,14 @@
+class cell{
+private:    
+    int xPos;
+    int yPos;
+     bool isInitialized = false;
+     bool topBorder = true;
+     bool rightBorder = true;
+     bool bottomBorder = true;
+     bool leftBorder = true;
+     bool isTraversed = false;
+
+     bool isGoal = false;
+
+}
