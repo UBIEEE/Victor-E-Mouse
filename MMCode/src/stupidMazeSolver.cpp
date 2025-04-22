bbcb3094087901bdad5d4562493c4b62e8ecf2b8
Haley Harblin <hpharbli@buffalo.edu>
2025-04-22 17:48:48 -0400

    stupidMazeSolver

---
+++ MMCode/src/stupidMazeSolver.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include <sensor.h>
+#include <motorEncoder.h> 
+
+//Getting Sensor Data 
+
+while(true){
+    if (right.getReading() < 10 && left.getReading() < 10){
+        //move forward
+    }
+    else if (right.getReading() < 10 && middle.getReading()){
+        //turn left 
+    }
+    else{
+        //turn right
+    }
+}
